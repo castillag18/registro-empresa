dee25fdaca4f09fb7ce0e11c2723a157b2a6fb84
unknown <orlandojs199918@gmail.com>
2018-03-15 12:45:16 -0500

    crud

---
+++ src/controller/empresaController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import controller.empresaController;
+import model.empresa;
+
+public class empresaController {
+   
+    empresa e1 = new empresa ();
+    
+     public void create (){
+        
+    }
+    
+    public void update (){
+        
+    }
+    
+    public void read (){
+        
+    }
+    
+    public void delete (){
+        
+    }
+     
+     
+}
